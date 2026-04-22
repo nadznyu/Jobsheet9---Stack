ed6d13c1f46cae1cabb6b7c9f3e883dc4ef6b0ad
nadznyu <nadiaalya0107@gmail.com>
2026-04-22 18:53:53 +0700

    Percobaan 2 (class StackTugas)

--- StackTugasMahasiswa16.java
+++ StackTugasMahasiswa16.java
@@ -73,4 +73,18 @@ public class StackTugasMahasiswa16 {
     public int count() {
         return top + 1;
     }
+
+    public String konversiDesimalkeBiner(int nilai) {
+        StackKonversi16 stack = new StackKonversi16();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
